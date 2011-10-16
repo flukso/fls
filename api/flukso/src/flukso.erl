c2923b7c5f6f1bf627ecf47369c529f1bde362b8
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2011-10-16 17:45:27 +0000

    [sql] group prepared sql statements in separate module

--- api/flukso/src/flukso.erl
+++ api/flukso/src/flukso.erl
@@ -29,19 +29,6 @@ ensure_started(App) ->
 	    ok
     end.
 
-mysql_prepare() ->
-    mysql:prepare(watchdog, <<"INSERT INTO watchdog (uid, type, message, variables, severity, location, hostname, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)">>),
-    mysql:prepare(permissions, <<"SELECT permissions FROM logger_tokens WHERE meter = ? AND token = ?">>),
-    mysql:prepare(master_token, <<"SELECT token FROM (logger_meters lm INNER JOIN logger_users lu ON lm.uid = lu.uid) WHERE meter = ? AND token = ?">>),
-    mysql:prepare(sensor_key, <<"SELECT sha FROM (logger_devices ld INNER JOIN logger_meters lm ON ld.device = lm.device) WHERE lm.meter = ?">>),
-    mysql:prepare(sensor_props, <<"SELECT uid, device, night FROM logger_meters WHERE meter = ?">>),
-    mysql:prepare(sensor_param, <<"SELECT access, type, function, class, voltage, current, phase, constant, enabled FROM logger_meters WHERE meter = ?">>),
-    mysql:prepare(sensor_update, <<"UPDATE logger_meters SET access = ?, night = ?, value = ? WHERE meter = ?">>),
-    mysql:prepare(sensor_config, <<"UPDATE logger_meters SET class = ?, type = ?, function = ?, voltage = ?, current = ?, phase = ?, constant = ?, enabled = ? WHERE meter = ?">>),
-    mysql:prepare(timezone, <<"SELECT timezone FROM users WHERE uid = ?">>),
-    mysql:prepare(device_key, <<"SELECT sha FROM logger_devices WHERE device = ?">>),
-    mysql:prepare(device_props, <<"SELECT sha, upgrade, resets FROM logger_devices WHERE device = ?">>),
-    mysql:prepare(device_update, <<"UPDATE logger_devices SET access = ?, version = ?, upgrade = ?, resets = ?, uptime = ?, memtotal = ?, memfree = ?, memcached = ?, membuffers = ? WHERE device = ?">>).
 
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the app for inclusion in a supervisor tree
@@ -50,7 +37,7 @@ start_link() ->
     ensure_started(crypto),
     ensure_started(erlrrd),
     ensure_started(mysql),
-    mysql_prepare(),
+    sql:prepare(),
     ensure_started(webmachine),
     flukso_sup:start_link().
 
@@ -61,7 +48,7 @@ start() ->
     ensure_started(crypto),
     ensure_started(erlrrd),
     ensure_started(mysql),
-    mysql_prepare(),
+    sql:prepare(),
     ensure_started(webmachine),
     application:start(flukso).
 
---
+++ api/flukso/src/sql.erl
@@ -0,0 +1,170 @@
+%% @author Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
+%% @copyright (C) 2011 Bart Van Der Meerssche
+%%%
+%%% This program is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU General Public License as published by
+%%% the Free Software Foundation, either version 3 of the License, or
+%%% (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%%
+%% @doc SQL prepared statements used in the REST API
+
+-module(sql).
+-author('Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>').
+
+-export([prepare/0]).
+
+-define(SQL_WATCHDOG,
+    <<"INSERT
+       INTO watchdog
+           (uid,
+            type,
+            message,
+            variables,
+            severity,
+            location,
+            hostname,
+            timestamp
+           )
+       VALUES
+           (?, ?, ?, ?, ?, ?, ?, ?)">>).
+
+-define(SQL_PERMISSIONS,
+    <<"SELECT 
+           permissions
+       FROM
+           logger_tokens
+       WHERE
+           meter = ? AND token = ?">>).
+
+-define(SQL_MASTER_TOKEN,
+    <<"SELECT
+           token
+       FROM
+           (logger_meters lm INNER JOIN logger_users lu ON lm.uid = lu.uid)
+       WHERE
+           meter = ? AND token = ?">>).
+
+-define(SQL_SENSOR_KEY,
+    <<"SELECT
+           sha
+       FROM
+           (logger_devices ld INNER JOIN logger_meters lm ON ld.device = lm.device)
+       WHERE
+           lm.meter = ?">>).
+
+-define(SQL_SENSOR_PROPS,
+    <<"SELECT
+           uid,
+           device,
+           night
+       FROM
+           logger_meters
+       WHERE
+           meter = ?">>).
+
+-define(SQL_SENSOR_PARAM,
+    <<"SELECT
+           access,
+           type,
+           function,
+           class,
+           voltage,
+           current,
+           phase,
+           constant,
+           enabled
+       FROM
+           logger_meters
+       WHERE
+           meter = ?">>).
+
+-define(SQL_SENSOR_UPDATE,
+    <<"UPDATE
+           logger_meters
+       SET
+           access = ?,
+           night = ?,
+           value = ?
+       WHERE
+           meter = ?">>).
+
+-define(SQL_SENSOR_CONFIG,
+    <<"UPDATE
+           logger_meters
+       SET
+           class = ?,
+           type = ?,
+           function = ?,
+           voltage = ?,
+           current = ?,
+           phase = ?,
+           constant = ?,
+           enabled = ?
+       WHERE
+           meter = ?">>).
+
+-define(SQL_TIMEZONE,
+    <<"SELECT
+           timezone
+       FROM
+           users
+       WHERE
+           uid = ?">>).
+
+-define(SQL_DEVICE_KEY,
+    <<"SELECT
+           sha
+       FROM
+           logger_devices
+       WHERE
+           device = ?">>).
+
+-define(SQL_DEVICE_PROPS,
+    <<"SELECT
+           sha,
+           upgrade,
+           resets
+       FROM
+           logger_devices
+       WHERE
+           device = ?">>).
+
+-define(SQL_DEVICE_UPDATE,
+    <<"UPDATE
+           logger_devices
+       SET
+           access = ?,
+           version = ?,
+           upgrade = ?,
+           resets = ?,
+           uptime = ?,
+           memtotal = ?,
+           memfree = ?,
+           memcached = ?,
+           membuffers = ?
+       WHERE
+           device = ?">>).
+
+
+% TODO: use a binary:replace to clean up \n's in the query strings
+prepare() ->
+    mysql:prepare(watchdog, ?SQL_WATCHDOG),
+    mysql:prepare(permissions, ?SQL_PERMISSIONS),
+    mysql:prepare(master_token, ?SQL_MASTER_TOKEN),
+    mysql:prepare(sensor_key, ?SQL_SENSOR_KEY),
+    mysql:prepare(sensor_props, ?SQL_SENSOR_PROPS),
+    mysql:prepare(sensor_param, ?SQL_SENSOR_PARAM),
+    mysql:prepare(sensor_update, ?SQL_SENSOR_UPDATE),
+    mysql:prepare(sensor_config, ?SQL_SENSOR_CONFIG),
+    mysql:prepare(timezone, ?SQL_TIMEZONE),
+    mysql:prepare(device_key, ?SQL_DEVICE_KEY),
+    mysql:prepare(device_props, ?SQL_DEVICE_PROPS),
+    mysql:prepare(device_update, ?SQL_DEVICE_UPDATE).
