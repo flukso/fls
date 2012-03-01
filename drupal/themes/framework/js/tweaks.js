eefc1a1f808d1f3929f8714c6db4c2328264421e
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2012-03-01 21:27:27 +0000

    [theme] substitute drupal messages for bootstrap alerts

--- drupal/themes/framework/js/tweaks.js
+++ drupal/themes/framework/js/tweaks.js
@@ -1,3 +1,25 @@
 $(function() {
-    $("table").addClass("table");
+    /* add bootstrap table formatting to each table element
+     * but exclude uc's delivery and billing address panes
+     */
+    $("table")
+        .not($("div.address-pane-table")
+        .children("table"))
+        .addClass("table");
+
+    /* substitute drupal messages for bootstrap alerts
+     * since bootstrap.js require jquery 1.7.1, we cannot
+     * use bootstrap's alert dismissal feature
+     */ 
+    $("div.messages.status")
+        .removeClass("messages status")
+        .addClass("alert alert-success");
+
+    $("div.messages.warning")
+        .removeClass("messages warning")
+        .addClass("alert");
+
+    $("div.messages.error")
+        .removeClass("messages error")
+        .addClass("alert alert-error");
 });
